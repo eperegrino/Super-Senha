615ff35f8086d327f7b2b7f21884e53589ace248
eperegrino <epps@cesar.school>
2019-11-09 09:07:18 -0300

    Initial commit

---
+++ Super Senha/Super Senha/PasswordGeneratorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PasswordGeneratorViewController.swift
+//  Super Senha
+//
+//  Created by Eduardo Peregrino on 08/11/19.
+//  Copyright © 2019 Eduardo Peregrino. All rights reserved.
+//
+
+import UIKit
+
+class PasswordGeneratorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Super Senha/Super Senha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Super Senha
+//
+//  Created by Eduardo Peregrino on 08/11/19.
+//  Copyright © 2019 Eduardo Peregrino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
